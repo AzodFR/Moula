2022cdb0797a12cf7bb9301041b82abcf4e9d5d2
AzodFR <azod.contact@gmail.com>
2021-03-30 16:28:56 +0200

    approve, allowance, tarnsferFrom

--- contracts/MoulaToken.sol
+++ contracts/MoulaToken.sol
@@ -15,7 +15,16 @@ contract MoulaToken
         uint256 _value
     );
 
+    event Approval
+    (
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+
     mapping(address => uint256) public balanceOf;
+
+    mapping(address => mapping(address => uint256)) public allowance;
 
     constructor(uint256 _initSupply) public
     {
@@ -25,12 +34,37 @@ contract MoulaToken
 
     function transfer(address _to, uint256 _value) public returns (bool)
     {
-        require(balanceOf[msg.sender] >= _value, 'revert: balance too low');
+        require(balanceOf[msg.sender] >= _value, "sender's balance is too low");
 
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
 
         emit Transfer(msg.sender, _to, _value);
+
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool)
+    {
+        allowance[msg.sender][_spender] = _value;
+
+        emit Approval(msg.sender, _spender, _value);
+
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool)
+    {
+        require(balanceOf[_from] >= _value, "sender's balance is too low");
+
+        require(allowance[_from][msg.sender] >= _value, "spender is not allowed to spend this much");
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        allowance[_from][msg.sender] -= _value;
+
+        emit Transfer(_from, _to, _value);
 
         return true;
     }
--- test/MoulaToken.js
+++ test/MoulaToken.js
@@ -37,7 +37,7 @@ contract('MoulaToken', function(accounts)
             tokenInstance = instance;
             return tokenInstance.transfer.call(accounts[1], 999999);
         }).then(assert.fail).catch(function(error) {
-            assert(error.message.indexOf('revert') >= 0, 'error message must contain revert');
+            assert(error.message.indexOf("sender's balance is too low") >= 0, 'error message must contain revert');
             return tokenInstance.transfer.call(accounts[1], 10000, { from: accounts[0] });
         }).then(function(success) {
             assert.equal(success, true, 'check return boolean');
@@ -57,4 +57,60 @@ contract('MoulaToken', function(accounts)
         });
     });
 
+    it('approves token for delegated transfer', function() {
+        return MoulaToken.deployed().then(function(instance) {
+            tokenInstance = instance;
+            return tokenInstance.approve.call(accounts[1], 100);
+        }).then(function(success) {
+            assert.equal(success, true, 'check return value');
+            return tokenInstance.approve(accounts[1], 100, { from: accounts[0] });
+        }).then(function(receipt) {
+            assert.equal(receipt.logs.length, 1, 'triggers one event');
+            assert.equal(receipt.logs[0].event, 'Approval', 'chekc the name of Event');
+            assert.equal(receipt.logs[0].args._owner, accounts[0], 'check owner');
+            assert.equal(receipt.logs[0].args._spender, accounts[1], 'check spender');
+            assert.equal(receipt.logs[0].args._value, 100, 'check value');
+            return tokenInstance.allowance(accounts[0], accounts[1]);
+        }).then(function(allowance) {
+            assert.equal(allowance.toNumber(), 100, 'store the allowance for delegated transfer');
+        });
+    });
+
+    it('handles delegated transfers', function() {
+        return MoulaToken.deployed().then(function(instance) {
+            tokenInstance = instance;
+            fromAccount = accounts[2];
+            toAccount = accounts[3];
+            spendingAccount = accounts[4];
+            return tokenInstance.transfer(fromAccount, 100, { from: accounts[0] });
+        }).then(function(receipt) {
+            return tokenInstance.approve(spendingAccount, 15, { from: fromAccount});
+        }).then(function(receipt) {
+            return tokenInstance.transferFrom(fromAccount, toAccount, 500, { from: spendingAccount });
+        }).then(assert.fail).catch(function(error) {
+            assert(error.message.indexOf("sender's balance is too low") >= 0, 'can\'t send more than balance');
+            return tokenInstance.transferFrom(fromAccount, toAccount, 50, { from: spendingAccount });
+        }).then(assert.fail).catch(function(error) {
+            assert(error.message.indexOf("spender is not allowed to spend this much") >= 0, 'can\'t spend this much');
+            return tokenInstance.transferFrom.call(fromAccount, toAccount, 10, { from: spendingAccount });
+        }).then(function(success) {
+            assert.equal(success, true, 'goo transfer from');
+            return tokenInstance.transferFrom(fromAccount, toAccount, 10, { from: spendingAccount });
+        }).then(function(receipt) {
+            assert.equal(receipt.logs.length, 1, 'triggers one event');
+            assert.equal(receipt.logs[0].event, 'Transfer', 'chekc the name of Event');
+            assert.equal(receipt.logs[0].args._from, fromAccount, 'check sender');
+            assert.equal(receipt.logs[0].args._to, toAccount, 'check receiver');
+            assert.equal(receipt.logs[0].args._value, 10, 'check value');
+            return tokenInstance.balanceOf(fromAccount);
+        }).then(function(from) {
+            assert.equal(from, 90, 'check new from balance');
+            return tokenInstance.balanceOf(toAccount);
+        }).then(function(to) {
+            assert.equal(to, 10, 'check new to balance');
+            return tokenInstance.allowance(fromAccount, spendingAccount);
+        }).then(function(allowance) {
+            assert.equal(allowance, 5, 'check updated allowance');
+        });
+    });
 })
